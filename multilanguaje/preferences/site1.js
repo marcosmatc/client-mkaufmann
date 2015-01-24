dee86020cbbd89c5cee69fc00384c670a391fd22
marcosmatc <marcosmatc@gmail.com>
2015-01-24 17:12:02 -0430

    Update site1.js

--- multilanguaje/preferences/site1.js
+++ multilanguaje/preferences/site1.js
@@ -1,1 +1,25 @@
-var pref;
+$(window).load(function() {
+
+	/*****************************************/
+	/*****Generación de Grupos de Textos******/
+	 textGroupArray[textGroupArray.length] = new TextGroup('Español');
+	 textGroupArray[textGroupArray.length] = new TextGroup('Inglés');
+	 textGroupArray[textGroupArray.length] = new TextGroup('italiano');
+
+
+
+});
+
+$(window).load(function() {
+
+	/*****************************************/
+	/*********Generación de Textos************/
+	 elementsArray[0] = 'HTML:eq(0) > BODY:eq(0) > DIV:eq(1) > H3:eq(0)';
+	 elementsArray[1] = 'HTML:eq(0) > BODY:eq(0) > H3:eq(0)';
+	 elementsArray[2] = 'HTML:eq(0) > BODY:eq(0) > DIV:eq(2) > DIV:eq(1) > SPAN:eq(0)';
+	 multiTextArray[0] = ['aaaaaaaaa','bbbbbbbbbb'];
+	 multiTextArray[1] = ['nnnnnn','mmmmmmmmm'];
+	 multiTextArray[2] = ['','','span en italiano'];
+
+
+});
